cc24df733e4a69696281f593600a454874bbf711
dongbing <2930838112@qq.com>
2023-05-22 11:01:46 +0800

    我在Person类中增加了ege属性

---
+++ TestMoudle01/src/com/dongbing/Person.java
@@ -0,0 +1,12 @@
+package com.dongbing;
+
+/**
+ * @Auther: DongBing
+ * @Date: 2023/5/22 - 05 - 22 - 11:00
+ * @Description: com.dongbing
+ * @version: 1.0
+ */
+public class Person {
+    private int age;
+}
+
